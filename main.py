1a22abbe8cce1149edd9cf6019137afd80f83c32
101ammarAh <ammarah5683@gmail.com>
2024-08-21 12:10:03 +0500

    completed parse_embeddings

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-# main.py
 import torch
+from sentence_transformers import SentenceTransformer
 
 # Load the embeddings once
 prod2prod_embeddings = torch.load('path/to/main_model_combined_embeddings.pt')
+
+# Load the model once
+model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
