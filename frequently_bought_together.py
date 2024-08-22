7ff9113cf2e51c53ec3a21fd6326b9a39afcf5df
101ammarAh <ammarah5683@gmail.com>
2024-08-22 15:13:26 +0500

    copied Aimas function for fpgrowth based frequently bought together

--- frequently_bought_together.py
+++ frequently_bought_together.py
@@ -3,5 +3,10 @@ from mlxtend.frequent_patterns import fpgrowth, association_rules
 from mlxtend.preprocessing import TransactionEncoder
 from scipy.sparse import csr_matrix, vstack
 
-def get_frequently_bought_products():
-    return
+
+# Function to get recommendations based on association rules
+def get_frequently_bought_products(cart_items, rules, top_n=3):
+    cart_items_set = frozenset(cart_items)
+    recommendations = rules[rules['antecedents'].apply(lambda x: cart_items_set.issubset(x))]
+    top_recommendations = recommendations[['consequents', 'confidence', 'lift']].sort_values(by='confidence', ascending=False).head(top_n)
+    return top_recommendations
