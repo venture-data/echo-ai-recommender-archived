4886633856a71273aafffe69fa59c6cd01fa9c49
101ammarAh <ammarah5683@gmail.com>
2024-08-22 12:52:18 +0500

    renaming files

--- frequently-bought-together.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from mlxtend.frequent_patterns import fpgrowth, association_rules
-from mlxtend.preprocessing import TransactionEncoder
-from scipy.sparse import csr_matrix, vstack
-
-def get_frequently_bought_products():
-    return
