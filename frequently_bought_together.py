5d123859c27b98ddb3c5988ac71d4b820a00bb22
101ammarAh <ammarah5683@gmail.com>
2024-08-22 15:18:49 +0500

    adjusted libraries

--- frequently_bought_together.py
+++ frequently_bought_together.py
@@ -1,9 +1,3 @@
-import pandas as pd
-from mlxtend.frequent_patterns import fpgrowth, association_rules
-from mlxtend.preprocessing import TransactionEncoder
-from scipy.sparse import csr_matrix, vstack
-
-
 # Function to get recommendations based on association rules
 def get_frequently_bought_products(cart_items, rules, top_n=3):
     cart_items_set = frozenset(cart_items)
