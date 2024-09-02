5167acb69d9a6f9bf433a62308df5ec77597b5f9
101ammarAh <ammarah5683@gmail.com>
2024-09-02 15:54:50 +0500

    added colab based paths

--- data_loader.py
+++ data_loader.py
@@ -2,8 +2,8 @@ import pandas as pd
 import torch
 from sentence_transformers import SentenceTransformer
 
-prod2prod_embeddings = torch.load('path/to/main_model_combined_embeddings.pt')
+prod2prod_embeddings = torch.load('/content/drive/MyDrive/recommender_system/Ammar Embeddings/semantic_model_combined_embeddings.p')
 model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
-products_with_ratings_aisle_department = pd.read_csv('path_to_your_csv_file.csv')
+products_with_ratings_aisle_department = pd.read_csv('/content/drive/MyDrive/recommender_system/Embeddings/product_random_normalized_ratings.csv')
 users_with_orders = pd.read_csv('path_to_your_csv_file.csv')
