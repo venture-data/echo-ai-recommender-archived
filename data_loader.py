7039862b7b089895f0966f246fdba46037712b25
101ammarAh <ammarah5683@gmail.com>
2024-09-02 16:06:31 +0500

    added t

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,7 @@ import pandas as pd
 import torch
 from sentence_transformers import SentenceTransformer
 
-prod2prod_embeddings = torch.load('/content/drive/MyDrive/recommender_system/Ammar Embeddings/semantic_model_combined_embeddings.p')
+prod2prod_embeddings = torch.load('/content/drive/MyDrive/recommender_system/Ammar Embeddings/semantic_model_combined_embeddings.pt')
 model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
 products_with_ratings_aisle_department = pd.read_csv('/content/drive/MyDrive/recommender_system/Embeddings/product_random_normalized_ratings.csv')
