0b07722fcbbcdd1b5d6530f169470a474154f038
101ammarAh <ammarah5683@gmail.com>
2024-09-02 15:04:57 +0500

    mv embeddings and model loading to data_loader

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-import torch
-from sentence_transformers import SentenceTransformer
 
-# Load the embeddings once
-prod2prod_embeddings = torch.load('path/to/main_model_combined_embeddings.pt')
 
-# Load the model once
-model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
