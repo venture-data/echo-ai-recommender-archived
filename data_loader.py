de74740125c842287e5bfe8d760c6736acb7c0e9
101ammarAh <ammarah5683@gmail.com>
2024-09-18 17:02:01 +0500

    added loadig

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,6 @@
-import pandas as pd
 import torch
+import pickle
+import pandas as pd
 from sentence_transformers import SentenceTransformer
 
 prod2prod_embeddings = torch.load('/content/drive/MyDrive/recommender_system/Ammar Embeddings/semantic_model_combined_embeddings.pt')
@@ -10,7 +11,8 @@ department_embedding = torch.load("")
 
 model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
-
-
 products_with_ratings_aisle_department = pd.read_csv('/content/drive/MyDrive/recommender_system/Embeddings/product_random_normalized_ratings.csv')
 # users_with_orders = pd.read_csv('path_to_your_csv_file.csv')
+
+with open('/content/drive/MyDrive/recommender_system/Embeddings/group_association_rules_dic.pkl', 'rb') as f:
+    group_association_rules_dic = pickle.load(f)
