f9c23259af89f58eabf7dfc80770a84cb5af4fdc
101ammarAh <ammarah5683@gmail.com>
2024-09-19 10:47:14 +0500

    added paths

--- data_loader.py
+++ data_loader.py
@@ -13,7 +13,10 @@ prod2prod_embeddings_path = os.getenv('PROD2PROD_EMBEDDINGS')
 product_name_embedding_path = os.getenv('PRODUCT_NAME_EMBEDDING')
 aisle_embedding_path = os.getenv('AISLE_EMBEDDING')
 department_embedding_path = os.getenv('DEPARTMENT_EMBEDDING')
-products_with_ratings_path = os.getenv('PRODUCTS_WITH_RATINGS')
+
+products_with_ratings_path = os.getenv('PRODUCTS_WITH_RATINGS_DF')
+user_id_cluster_path = os.getenv('USER_ID_WITH_CLUSTER_DF')
+
 group_association_rules_path = os.getenv('GROUP_ASSOCIATION_RULES')
 rules_freq_bought_path = os.getenv('RULES_FREQ_BOUGHT')
 
@@ -26,6 +29,7 @@ department_embedding = torch.load(department_embedding_path)
 model_semantic_search = SentenceTransformer('multi-qa-mpnet-base-cos-v1')
 
 products_with_ratings_aisle_department = pd.read_csv(products_with_ratings_path)
+user_id_cluster = pd.read_csv(user_id_cluster_path)
 
 with open(group_association_rules_path, 'rb') as f:
     group_association_rules_dic = pickle.load(f)
